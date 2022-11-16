96f81351b100657dafef1313a8de88450882c305
Warren Dubery <warrendubery@protonmail.ch>
2022-11-16 16:42:43 +0000

    fixed hero and footer styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,40 +7,38 @@ import { AiOutlineInstagram } from "react-icons/ai";
 const Footer = () => {
   return (
     <section className='footer'>
-      <div className='footer__container-left'>
-        <div className='footer__logo'>
-          <img src={logo} alt='LJT logo' />
-        </div>
-      </div>
+      <div className='footer__container'>
+        <ul>
+          <li>
+            <div className='footer__logo'>
+              <img src={logo} alt='LJT logo' />
+            </div>
+          </li>
 
-      <div className='footer__container-right'>
-        <div className='footer__links'>
-          <ul>
-            <li>
-              <div className='footer__contacts'>
-                <p>TEL: 07939-961667</p>
-              </div>
-            </li>
-            <li>
-              <a
-                href='https://www.facebook.com/'
-                target='_blank'
-                rel='noreferrer'
-              >
-                <FiFacebook />
-              </a>
-            </li>
-            <li>
-              <a
-                href='https://www.instagram.com/'
-                target='_blank'
-                rel='noreferrer'
-              >
-                <AiOutlineInstagram />
-              </a>
-            </li>
-          </ul>
-        </div>
+          <li>
+            <div className='footer__tel'>
+              <p>TEL: 07939-961667</p>
+            </div>
+          </li>
+          <li>
+            <a
+              href='https://www.facebook.com/'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <FiFacebook />
+            </a>
+          </li>
+          <li>
+            <a
+              href='https://www.instagram.com/'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <AiOutlineInstagram />
+            </a>
+          </li>
+        </ul>
       </div>
     </section>
   );
