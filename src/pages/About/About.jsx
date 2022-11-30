4e78256b96263ca88c8abe1eac7d98de7bd5c546
Warren Dubery <warrendubery@protonmail.ch>
2022-11-30 10:15:53 +0000

    add About content

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,4 +1,5 @@
 import profile from "../../assets/images/profile.jpeg";
+import Footer from "../../components/Footer/Footer";
 
 import "./About.scss";
 
@@ -8,7 +9,7 @@ const About = () => {
       <section className='about__head'>
         <div className='about__head-container'>
           <div className='about__head-text'>
-            <h2 className='about__head-title'>A Little Bit About Us...</h2>
+            <h2 className='about__head-title'>A Little Bit About Me...</h2>
           </div>
           <div className='about__head-img'>
             <img src={profile} alt='profile LJT Carpentry' />
@@ -18,20 +19,22 @@ const About = () => {
       <section className='about__body'>
         <div className='about__body-container'>
           <p className='about__body-text'>
-            Lloyd of LJT Carpentry has been perfecting his craft for over 15
-            years. He can produce high-end carpentry, cabinet-making and home
-            improvement services. He is a qualified carpenter and has a
-            certificate in cabinet-making. He is also a qualified painter and
-            decorator. He has a passion for his work and takes pride in his
-            workmanship. He is a perfectionist and will not leave a job until it
-            is completed to the highest standard. He is a hard worker and will
-            always go the extra mile to ensure that his customers are happy with
-            the work he has done. He is a friendly and approachable person and
-            will always listen to his customers and work with them to achieve
-            their desired outcome.
+            Hi, I'm Lloyd of LJT Carpentry. I have been perfecting my craft for
+            over 15 years. I pride myself in producing high-end carpentry,
+            cabinet-making projects and home improvements. I have passion for my
+            work and take pride in my workmanship. I am a bit of a perfectionist
+            and will not leave a job until it is completed to the highest
+            standard.
+          </p>
+          <p>
+            I will always go the extra mile to ensure that my customers are
+            happy with the work I have done. People say that I am a friendly and
+            approachable person. I will always listen to my customers and work
+            with them to achieve their desired outcome.
           </p>
         </div>
       </section>
+      <Footer />
     </section>
   );
 };
