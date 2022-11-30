e57723e86f7e8058623af07afbd20e5e3fbe8b9d
Warren Dubery <warrendubery@protonmail.ch>
2022-11-30 13:10:42 +0000

    edit services

--- src/components/Offers/Services.jsx
+++ src/components/Offers/Services.jsx
@@ -12,36 +12,50 @@ const Offers = () => {
             <div className='offers__card'>
               <h4 className='offers__card-title'>Kitchens</h4>
               <p className='offers__card-text'>
-                Planning, Design, Supply and Installation
+                Unique and handcrafted, individually designed and built to
+                order. A high-quality, bespoke kitchen can be timeless addition
+                to your home, perfectly displaying your individual style.
               </p>
             </div>
             <div className='offers__card'>
               <h4 className='offers__card-title'>Doors</h4>
               <p className='offers__card-text'>
-                Internal, External, Bi-Fold, French, Patio, Stable, Garage doors
+                Made to measeure doors of all varieties (internal, External,
+                Bi-Fold, French, Patio, Stable, Garage doors etc.) Designed to
+                meet your exacting requirements in different styles and
+                finishes.
               </p>
             </div>
             <div className='offers__card'>
               <h4 className='offers__card-title'>Stairs</h4>
               <p className='offers__card-text'>
-                New installations and refurbishments
+                New installations and refurbishments. Every staircase is unique
+                and represents a opportunity to create something individual and
+                to a customers taste and budget, whether modern or contemporary.
               </p>
             </div>
             <div className='offers__card'>
               <h4 className='offers__card-title'>Fitted Wardrobes</h4>
               <p className='offers__card-text'>
-                Planning, Design and Installation
+                Planning, Design and Installation. There are endless
+                possibilities when designing bespoke cabinetry. We can create a
+                unique and stylish storage solution to suit your needs that will
+                truly make your house, your home.
               </p>
             </div>
             <div className='offers__card'>
               <h4 className='offers__card-title'>Decking and Flooring</h4>
               <p className='offers__card-text'>
-                New installations and refurbishments
+                New installations and refurbishments. Wooden flooring and
+                decking is a beautiful and timeless addition to any home or
+                garden.
               </p>
             </div>
           </div>
         </div>
       </section>
+      <hr />
+      <hr />
     </>
   );
 };
